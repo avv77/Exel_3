f72837e43c9997a239bc7667a9ad017368416b74
Владимир Абинов <mag7777@mail.ru>
2021-07-27 08:20:05 +0300

    черновик

--- 1.py
+++ 1.py
@@ -13,4 +13,14 @@ for i in range(1, quantity_row + 1):
             if k == '`':
                 c = b.replace('`', '')
                 d = sheet.cell(row=i, column=j, value=c)
+sheet.insert_cols(idx=76)  # добавляем столбец "Страны"
+sheet['BX1'] = 'Страны'
+quantity_row = sheet.max_row
+quantity_column = sheet.max_column
+for i in range(2, quantity_row + 1):
+    i = str(i)
+    a = sheet['BX' + i]
+    i = int(i)
+    a = str('=ВПР(RC[-1];[Классификатор_стран.xlsx]Лист2!C1:C2;2;ЛОЖЬ)')
+    d = sheet.cell(row=i, column=76, value=a)
 wb.save('D:\\Работа\\Химия\\Хлорид железа\\Импорт_хлорид железа1_1.xlsx')
--- 2.py
+++
@@ -1,6 +0,0 @@
-a = '`1`'
-b = 'fdsgfargareg'
-for i in a:
-    if i == '`':
-        b = a.replace('`', '')
-print(b)
